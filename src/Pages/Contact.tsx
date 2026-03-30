2f040db9b591465e5089f70e688a9b7066ca5eca
MinalYadav <meenalyadav13@gmail.com>
2026-03-30 18:03:29 +0530

    bug fixed

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Navbar from "../components/Navbar";
 import { motion, type Variants } from "framer-motion";
 import Footer from "../components/Footer";
--- src/Pages/Portfolio.tsx
+++ src/Pages/Portfolio.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import { motion, type Variants } from "framer-motion";
 
